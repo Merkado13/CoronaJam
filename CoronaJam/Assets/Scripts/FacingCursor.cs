f95c697e7efb7e9ed74577f821e416786fd2fc69
Merkado13 <Merkado13@users.noreply.github.com>
2020-03-17 12:16:22 +0100

    Face To Cursor

---
+++ CoronaJam/Assets/Scripts/FacingCursor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacingCursor : MonoBehaviour
+{
+    
+    private SpriteRenderer renderer;
+    [SerializeField] private Vector3 cursorPos;
+    [SerializeField] private Camera curentCamera;
+
+    private void Awake()
+    {
+        renderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        faceToCursor();
+    }
+
+    private void faceToCursor(){
+        cursorPos = curentCamera.ScreenToWorldPoint(Input.mousePosition);
+        float xOffset = cursorPos.x - transform.position.x;
+        renderer.flipX = xOffset >= 0;
+    }
+}
