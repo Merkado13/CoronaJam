776d14f0b06ed8d4ec96b10ea98e2a1dd8b49d64
Pablo Sánchez <59802775+PabloSanchezTrujillo@users.noreply.github.com>
2020-03-18 02:40:22 +0100

    2D pathfinding

---
+++ CoronaJam/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpawner : MonoBehaviour
+{
+    #region variables
+
+    [SerializeField] private GameObject enemy;
+    [SerializeField] private GameObject[] windows;
+
+    #endregion variables
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        GameObject enemyClone = Instantiate(enemy);
+        enemyClone.GetComponent<Enemy_FSM>().Windows = windows;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+}
---
+++ CoronaJam/Assets/Scripts/Enemies/Enemy_FSM.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AnimatorStateMachineUtil;
+
+public class Enemy_FSM : MonoBehaviour
+{
+    #region variables
+
+    public GameObject[] Windows { get; set; }
+
+    private Rigidbody2D rigidbody;
+
+    #endregion variables
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+    }
+
+    #region FSM_SearchWindow
+
+    [StateEnterMethod("Base.Search window")]
+    public void EnterSearchWindow()
+    {
+        print("Busco ventana");
+        //rigidbody.velocity = new Vector2(-1, 0);
+    }
+
+    #endregion FSM_SearchWindow
+}
