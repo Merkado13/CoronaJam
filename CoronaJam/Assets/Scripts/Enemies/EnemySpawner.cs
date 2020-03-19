f8a6b3c895b060132ab284f3d93b2d3d59d24a23
Pablo Sánchez <59802775+PabloSanchezTrujillo@users.noreply.github.com>
2020-03-19 03:08:44 +0100

    Enemy finished

--- CoronaJam/Assets/Scripts/Enemies/EnemySpawner.cs
+++ CoronaJam/Assets/Scripts/Enemies/EnemySpawner.cs
@@ -6,20 +6,22 @@ public class EnemySpawner : MonoBehaviour
 {
     #region variables
 
+    [SerializeField] private SAP2D.SAP2DPathfindingConfig pathfindingConfig;
     [SerializeField] private GameObject enemy;
-    [SerializeField] private GameObject[] windows;
+    [SerializeField] private Transform[] windowsOutside;
+    [SerializeField] private Transform[] windowsInside;
+    [SerializeField] private GameObject toiletPaper;
 
     #endregion variables
 
     // Start is called before the first frame update
     private void Start()
     {
-        GameObject enemyClone = Instantiate(enemy);
-        enemyClone.GetComponent<Enemy_FSM>().Windows = windows;
-    }
-
-    // Update is called once per frame
-    private void Update()
-    {
+        GameObject enemyClone = Instantiate(enemy, transform.position, transform.rotation);
+        Enemy_FSM enemyFSM = enemyClone.GetComponent<Enemy_FSM>();
+        enemyFSM.WindowsOutside = windowsOutside;
+        enemyFSM.WindowsInside = windowsInside;
+        enemyFSM.PathfindingConfig = pathfindingConfig;
+        enemyFSM.ToiletPaper = toiletPaper;
     }
 }
--- CoronaJam/Assets/Scripts/Enemies/Enemy_FSM.cs
+++ CoronaJam/Assets/Scripts/Enemies/Enemy_FSM.cs
@@ -7,31 +7,112 @@ public class Enemy_FSM : MonoBehaviour
 {
     #region variables
 
-    public GameObject[] Windows { get; set; }
+    public Transform[] WindowsOutside { get; set; }
+    public Transform[] WindowsInside { get; set; }
+    public SAP2D.SAP2DPathfindingConfig PathfindingConfig { get; set; }
+    public GameObject ToiletPaper { get; set; }
 
-    private Rigidbody2D rigidbody;
+    [SerializeField] private float timeToBreakWindow;
+
+    private SAP2D.SAP2DAgent navAgent;
+    private Animator enemyFSM;
+    private Window targetWindow;
 
     #endregion variables
 
     // Start is called before the first frame update
     private void Start()
     {
-        rigidbody = GetComponent<Rigidbody2D>();
+        navAgent = GetComponent<SAP2D.SAP2DAgent>();
+        enemyFSM = GetComponent<Animator>();
+
+        navAgent.Config = PathfindingConfig;
     }
 
-    // Update is called once per frame
-    private void Update()
-    {
-    }
-
-    #region FSM_SearchWindow
+    #region StateMachine
 
     [StateEnterMethod("Base.Search window")]
     public void EnterSearchWindow()
     {
-        print("Busco ventana");
-        //rigidbody.velocity = new Vector2(-1, 0);
+        SearchWindow(WindowsOutside);
     }
 
-    #endregion FSM_SearchWindow
+    public void SearchWindow(Transform[] windows)
+    {
+        float actualDistance = 100000f;
+
+        foreach(Transform windowPos in windows) {
+            float calculatedDistance = Vector2.Distance(transform.position, windowPos.position);
+            if(calculatedDistance < actualDistance) {
+                navAgent.Target = windowPos;
+                actualDistance = calculatedDistance;
+            }
+        }
+    }
+
+    [StateEnterMethod("Base.Break window")]
+    public void EnterBreakWindow()
+    {
+        InvokeRepeating("BreakWindow", timeToBreakWindow, timeToBreakWindow);
+    }
+
+    private void BreakWindow()
+    {
+        if(targetWindow.Barricades > 0) {
+            targetWindow.Barricades--;
+        }
+        else {
+            CancelInvoke("BreakWindow");
+            enemyFSM.SetTrigger("WindowBroken");
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.GetComponent<Window>() != null) {
+            enemyFSM.SetTrigger("GetToWindow");
+            enemyFSM.SetTrigger("RunAway");
+            targetWindow = collision.GetComponent<Window>();
+        }
+
+        if(collision.GetComponent<ToiletPaper>() != null) {
+            collision.transform.parent = transform;
+            collision.transform.position = Vector3.zero;
+            enemyFSM.SetTrigger("GetToWindowExit");
+        }
+    }
+
+    [StateEnterMethod("Base.Go for toilet paper")]
+    public void EnterToiletPaper()
+    {
+        enemyFSM.ResetTrigger("GetToWindow");
+        enemyFSM.ResetTrigger("RunAway");
+        navAgent.Target = ToiletPaper.transform;
+    }
+
+    [StateEnterMethod("Base.Search window exit")]
+    public void EnterSearchWindowExit()
+    {
+        SearchWindow(WindowsInside);
+        transform.GetChild(0).transform.position = Vector3.zero;
+    }
+
+    [StateEnterMethod("Base.Run away")]
+    public void EnterRunAway()
+    {
+        if(targetWindow.Barricades > 0) {
+            InvokeRepeating("BreakWindow", timeToBreakWindow, timeToBreakWindow);
+        }
+    }
+
+    [StateUpdateMethod("Base.Run away")]
+    public void UpdateRunAway()
+    {
+        if(targetWindow.Barricades == 0) {
+            navAgent.Target = targetWindow.transform.GetChild(0).transform;
+            //TODO: hacer que pierda el jugador
+        }
+    }
+
+    #endregion StateMachine
 }
