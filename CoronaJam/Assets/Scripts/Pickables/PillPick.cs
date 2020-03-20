2d4ed25288e29dcb2ecbd426557f10a2eb08e0f2
Merkado13 <Merkado13@users.noreply.github.com>
2020-03-20 22:43:33 +0100

    Pickables & Player Animation

---
+++ CoronaJam/Assets/Scripts/Pickables/PillPick.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ColorPill { BLUE, RED}
+
+public class Pill
+{
+    public ColorPill color;
+
+    public Pill(ColorPill color)
+    {
+        this.color = color;
+    }
+
+    public void UsePill(PlayerController player)
+    {
+        if(color == ColorPill.BLUE){
+
+
+        }else if(color == ColorPill.RED)
+        {
+
+        }
+
+    }
+}
+
+public class PillPick : MonoBehaviour, IPickable
+{
+
+    [SerializeField] private ColorPill color;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Pick(PlayerController player)
+    {
+        player.setPill(new Pill(color));
+        Destroy(gameObject);
+    }
+
+}
